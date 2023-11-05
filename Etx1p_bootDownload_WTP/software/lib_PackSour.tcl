5a73b6dabf38819fefbbad34cc46401a452f22ff
Ilya Ginzburg <ilya_g@rad.com>
2023-11-05 14:40:28 +0200

    Source lib_bc for UnregIdBarcode

--- Etx1p_bootDownload_WTP/software/lib_PackSour.tcl
+++ Etx1p_bootDownload_WTP/software/lib_PackSour.tcl
@@ -79,7 +79,7 @@ source Gui_Etx1pDnld.tcl
 source Lib_Gen_Etx1pDnld.tcl
 source Lib_DialogBox.tcl
 source Main_Etx1pDnld.tcl
-# source lib_bc.tcl
+source lib_bc.tcl
 
 source [info host]/init$gaSet(pair).tcl
 source [info host]/init.tcl
