90e1930af7d07da6d89ea943f7fed84bd64012cd
Ilya Ginzburg <ilya_g@rad.com>
2025-07-09 15:23:25 +0300

    Fix typo in RLWS::UpdateDB2 (SQliteAddLine)

--- Etx1p_bootDownload_WTP/software/lib_SQlite.tcl
+++ Etx1p_bootDownload_WTP/software/lib_SQlite.tcl
@@ -94,7 +94,7 @@ proc SQliteAddLine {} {
   for {set tr 1} {$tr <= 6} {incr tr} {
     #if [catch {UpdateDB $barcode $uut $hostDescription $date $tim-$gaSet(ButRunTime) $status $failTestsList $failReason $operator} res] {}
     #if [catch {RLWS::UpdateDB2 $barcode $uut $hostDescription $date $tim-$gaSet(ButRunTime) $status $failTestsList $failReason $operator $traceID $poNumber "" "" ""} res] {}
-    foreach {ret resTxt} [RLWS::UpdateDB2 $barcode $uut $hostDescription $date 
+    foreach {ret resTxt} [RLWS::UpdateDB2 $barcode $uut $hostDescription $date\ 
       $tim-$gaSet(ButRunTime) $status $failTestsList $failReason $operator \
       $traceID $poNumber [info host] "" ""] {}
     if {$ret!=0} {
